dc59c86561303cc795d29db0fade376c3480db22
Formagin <austin.formagin117@gmail.com>
2017-12-15 16:54:01 -0500

    Updated Movement

--- src/Movement.java
+++ src/Movement.java
@@ -3,4 +3,41 @@
 //Programmer Assigned: Austin
 
 public class Movement {
-}
+    private int charPosX;
+    private int charPosY;
+
+
+
+    ////////////////////////////////////////////////////////////////////////////////////////
+    //Change the values of X & Y
+    ////////////////////////////////////////////////////////////////////////////////////////
+
+    //Movement Right
+    public void moveRight(int currentPosX){
+        //Adjust the x value to become more positive
+        charPosX = currentPosX + 5;//Value will change after testing
+    }//End of moveRight
+
+    //Movement Left
+    public void moveLeft(int currentPosX){
+        //Adjust the x value to become more negative
+        charPosX = currentPosX - 5;//Value will change after testing
+    }//End of moveLeft
+
+    //Movement Up
+    public void moveUp(int currentPosY){
+        //Adjust the y value to become more positive
+        charPosX = currentPosY + 5;//Value will change after testing
+    }//end of MoveUp
+
+    //Movement Down
+    public void moveDown(int currentPosY){
+        //Adjust the y value to become more negative
+        charPosX = currentPosY - 5;//Value will change after testing
+    }//End of moveDown
+    ////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+    ////////////////////////////////////////////////////////////////////////////////////////
+}//End of Movement class
