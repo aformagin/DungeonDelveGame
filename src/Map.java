2a68112a6959ec35a783431ce8e478ac60126700
SpookySkelemen <generalflapjack8@gmail.com>
2018-03-14 12:22:03 -0400

    Commented the Map object

--- src/Map.java
+++ src/Map.java
@@ -5,7 +5,7 @@ public class Map {
 
     public Map (){
         tiles = new Tile [1][1];            // default Map is a 1x1 grid.
-    }
+    } // Map constructor
 
     public Map (int xSize, int ySize) {     // Map with specific size
         tiles = new Tile [xSize][ySize];
@@ -15,8 +15,8 @@ public class Map {
                     tiles [x][y] = new Tile();
                 } catch (IOException e) {
                     e.printStackTrace();
-                }
-            }
-        }
-    }
-}
+                } // try catch
+            } // for y
+        } // for x
+    } // Map constructor 2
+} // Map class
